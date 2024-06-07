9fdc5fea0f8f810482911c4e57e6fe2e72fb75b5
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2024-06-07 22:27:18 +0200

    added a stop background music feature

--- js/music.js
+++ js/music.js
@@ -1,9 +1,12 @@
 const song = document.getElementById('song');
-// Plays music when user presses 'P" and stops the music using 'S'
+// Plays music when user presses 'P" and stops the music using 'S' and 'M' pauses the music
 document.onkeydown = function(event) {
   if (event.key === 'p' || event.key === 'P') {
     song.play();
   } else if (event.key === 's' || event.key === 'S') {
+    song.load();
+  }
+    else if (event.key === 'm' || event.key === 'M') {
     song.pause();
   } else {
     alert('Background feature unavailable!');

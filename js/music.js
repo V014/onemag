531190aff49e25cab3579d8ffffd7eb753782370
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2024-07-08 21:48:55 +0200

    "Changed event listener from 'onkeypress' to 'onkeydown' in music.js"

--- js/music.js
+++ js/music.js
@@ -1,6 +1,6 @@
 const song = document.getElementById('song');
 // Plays music when user presses 'P" and stops the music using 'S' and 'M' pauses the music
-document.onkeypress = function(event) {
+document.onkeydown = function(event) {
   if (event.key === 'p' || event.key === 'P') {
     song.play();
   } else if (event.key === 's' || event.key === 'S') {

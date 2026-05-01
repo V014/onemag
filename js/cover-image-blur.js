d69e751a46475a1b523815ab046f7ea8540d5aa7
Wanga Kanjala <wangakanjala@gmail.com>
2026-05-01 18:21:03 +0200

    removed play music code and other non working code

--- js/cover-image-blur.js
+++
@@ -1,5 +0,0 @@
-// unblur cover image when it's fully loaded
-const coverImage = document.getElementById('cover-image');
-    coverImage.onload = function() {
-    coverImage.classList.remove('blur');
-}
